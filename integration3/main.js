351644c750345fe7ef3983a6bb063f702e869e65
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-01-21 17:34:01 +0100

    change:installed  grained.min.js + license package from github

--- integration3/main.js
+++ integration3/main.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+/*Source: Grained bg>  https://github.com/sarathsaleem/grained
+ * Grain generator:  http://sarathsaleem.github.io/grained/ */
+
+
+const init = () => {
+  const noiseOptions = {
+    animate: true,
+    patternWidth: 403,
+    patternHeight: 200,
+    grainOpacity: 0.6,
+    grainDensity: 1,
+    grainWidth: 1,
+    grainHeight: 1,
+  };
+  /* 
+  const $noise = document.querySelector('.bg__grained');  -  they do it themselves...*/
+  grained('#grained', noiseOptions);
+};
+init();
