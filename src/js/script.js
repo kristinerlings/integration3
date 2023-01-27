c201db6b84515b6337eab8fcf7a3e0c8263d1aa6
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-01-27 12:24:24 +0100

    testing audio - check if works when deployed

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
-const $menu = document.querySelector('.hamburger');
-const $nav = document.querySelector('.nav');
+const $playSound = document.querySelector('.btn__sound');
 
-$menu.addEventListener('click', function (e) {
-  $nav.classList.toggle('show_nav');
-});
+const playAudio = () => {
+  const audio = new Audio('assets/audio/245091__neilseggar__g-harmonica.wav');
+
+  audio.play();
+};
+
+const init = () => {
+  const $menu = document.querySelector('.hamburger');
+  const $nav = document.querySelector('.nav');
+
+  $menu.addEventListener('click', function (e) {
+    $nav.classList.toggle('show_nav');
+  });
+
+  $playSound.addEventListener('click', playAudio);
+};
+
+init();
