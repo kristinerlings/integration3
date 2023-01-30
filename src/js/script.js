f0afad6a5376c7509441552404dfdc8ab89ae107
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-01-30 01:06:22 +0100

    CSS: journey adjustments > +footsteps-mobile + JS>GSAP setup

--- src/js/script.js
+++ src/js/script.js
@@ -52,65 +52,96 @@ const intersectionObserver = () => {
 
 let currentIndex = 0;
 
-const containerChina = () =>{
-
-
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.about',
-    start: 'top 20%',
-    end: '+=5000',
-    scrub: 1,
-    pin: true,
-    markers: true,
-    markers: {
-      startColor: 'white',
-      endColor: 'green',
+const createTimeline = (trigger, start, end, scrub, pin) =>{
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: trigger,
+      start: start,
+      end: end,
+      scrub: scrub,
+      pin: pin,
+      markers: true,
+      markers: {
+        startColor: 'white',
+        endColor: 'green',
+      },
     },
-  },
-});
-
-  tl.set('.txt__wrp--sheng', {
-    autoAlpha: 0,
   });
-  tl.set('.title__china', {
-    autoAlpha: 0,
-  });
-  tl.set('.img__china', {
-    opacity: 0,
-  })
-  tl.to('.first', {
-   autoAlpha: 1,
-    duration: 2,
-  });
-  tl.to('.first', {
-    autoAlpha: 0,
-    duration: 2,
-  });
-   tl.to('.second', {
-     autoAlpha: 1,
-     duration: 2,
-   });
-   tl.to('.second', {
-     autoAlpha: 0,
-     duration: 2,
-   });
-    tl.to('.third', {
-      autoAlpha: 1,
-      duration: 2,
-    });
-    tl.to('.third', {
-      autoAlpha: 0,
-      duration: 2,
-    });
-   
-
+   return tl;
 }
 
-const largeScreenSize = () =>{
+const containerChina = () => {
+  let tl = createTimeline('.about', 'top 20%', '+5000', 1, true);
 
+  /*   rotation: '180deg', */
+
+  tl.set('.txt__wrp--sheng, .title__china, .img__sheng, .img__china--human', {
+    autoAlpha: 0,
+  });
+  tl.from('.img__china', {
+    scale: 1.5,
+    duration: 1,
+    easeOut: 'power2.in',
+  });
+  tl.to('.title__china', {
+    autoAlpha: 1,
+    duration: 1,
+  });
+  tl.to('.first', {
+    autoAlpha: 1,
+    duration: 1,
+  });
+  tl.to('.img__china--human', {
+    autoAlpha: 1,
+    duration: .5,
+  });
+  tl.to('.first', {
+    autoAlpha: 0,
+    duration: 2,
+  });
+  tl.to('.img__china--human, .img__china', {
+    autoAlpha: 0,
+    duration: 1,
+  });
+  tl.to('.img__sheng', {
+    autoAlpha: 1,
+    duration: .5,
+  });
+  tl.to('.second', {
+    autoAlpha: 1,
+    duration: 1,
+  });
+  tl.to('.second', {
+    autoAlpha: 0,
+    duration: 1,
+  });
+  tl.to('.third', {
+    autoAlpha: 1,
+    duration: 2,
+  });
+  tl.to('.third', {
+    autoAlpha: 0,
+    duration: 2,
+  });
+};
+
+const containerJourcney = () => {
+  let tl = createTimeline('.journey__container', 'top top', '+3000', .5, true);
+  
+  tl.set('.txt-wrap,.img__footstep, img__footstep-l2, img__footstep-l', {
+    autoAlpha: 0,
+  });
+  tl.to('.txt__first', { autoAlpha: 1, duraiton: 0.2});
+  tl.to('.img__footstep:nth-child(2)', { autoAlpha: 1, duraiton: 0.2 });
+  tl.to('.img__footstep:nth-child(3)', { autoAlpha: 1, duraiton: 0.2 });
+  tl.to('.txt-silkroad', { autoAlpha: 1, duraiton: 0.2 });
+  tl.to('.img__footstep:nth-child(5)', { autoAlpha: 1, duraiton: 0.2 });
+  tl.to('.img__footstep:nth-child(6)', { autoAlpha: 1, duraiton: 0.2 });
+  tl.to('.img__light--off', { autoAlpha: 0, duration: 0.1 });
+  tl.to('.img__light--on',{autoAlpha:1, display:'block', duration:0.1});
 }
 
+const largeScreenSize = () => {};
 
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -118,7 +149,7 @@ const init = () => {
     markers: { startColor: 'brown', endColor: 'brown' },
   });
 
-/* 
+  /* 
 
   smallMediaQuery.addListener(function(event) {
   if (smallMediaQuery.matches) {
@@ -134,9 +165,8 @@ const init = () => {
   });
  */
 
-  
-containerChina();
-
+  containerChina();
+  containerJourney();
 
   intersectionObserver();
   $playSound.addEventListener('click', toggleAudio);
